5c73bf6d433ea9f7104e71064f99da60f1d071bb
heyvats21 <singhvatsal59@gmail.com>
2022-09-08 10:36:54 +0530

    input and output talk

---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log("its working from app.js")
+
+
+// *******************Input & output talk**********
+
+// var userName=prompt("Give me your username")
+// alert("This is working "+userName)
