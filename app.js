8e5b504b407f99c6db8e14e06ae424230bbe8566
heyvats21 <singhvatsal59@gmail.com>
2022-09-22 21:25:58 +0530

    all final code
    publish all the codes

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-// console.log("its working from app.js")
+var btnTranslate = document.querySelector("#btn-translate");
+var btnClear = document.querySelector("#btn-clear");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text
+}
 
 
-// *******************Input & output talk**********
+//error handleling
+function errorHandler(error){
+    console.log("Error occured",error);
+    alert("Something went wrong with server : try again after some time");
+}
 
-// var userName=prompt("Give me your username")
-// alert("This is working "+userName)
+function clickHandler() {
+    outputDiv.innerText="Loading...";
+    var textInput = txtInput.value;
+    if(textInput==""){
+        outputDiv.innerHTML="Please enter some text!";
+    } else{
+        fetch(getTranslationURL(textInput))
+        .then(response => response.json())
+        // .then(json => console.log(json.contents.translated))
+        .then(json =>{
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+    }
+}
+
+// function clearHandler() {
+//     outputDiv.innerHTML="";
+//     txtInput.value="";
+// }
+
+btnTranslate.addEventListener("click", clickHandler);
+// btnClear.addEventListener("click", clearHandler);
